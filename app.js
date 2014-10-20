3d612c51ca6c67c0e16a2514c3e7651a11f27ee0
Serhii Kuts <sergeykuc@gmail.com>
2014-10-20 14:28:49 +0300

    view bugfix

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ require('./config/express')(app, config);
 var interval = 10*60000;
 setInterval(function(){
     "use strict";
+    console.log('grabbing data');
     request('http://car-price-watcher.herokuapp.com/grab', function(err,resp){
         console.log(err)
     })
@@ -31,6 +32,7 @@ setInterval(function(){
 
 setInterval(function(){
     "use strict";
+    console.log('sending email');
     request('http://car-price-watcher.herokuapp.com/email', function(err,resp){
         console.log(err)
     })
